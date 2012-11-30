dd33000199bc24907f5b73cda77cbf350c53fbd1
Jim McAndrew <jim@loc8.us>
2012-11-30 15:25:32 -0700

    added git messages to the command line

--- update.sh
+++ update.sh
@@ -1,3 +1,5 @@
 # This will go through all the vim bundles and update them
 
 git submodule foreach git pull origin master
+
+curl -Sso ~/dotfiles/bash/git-completion.bash https://raw.github.com/git/git/master/contrib/completion/git-completion.bash
