3c47089cfc82a28b2e792c447fcd16cd45c1da1b
Jim McAndrew <jim@loc8.us>
2012-12-01 15:52:33 -0700

    Fixed the text wrapping issues caused by the new bathe new bash prompt in the bashrc file

--- update.sh
+++ update.sh
@@ -3,3 +3,4 @@
 git submodule foreach git pull origin master
 
 curl -Sso ~/dotfiles/bash/git-completion.bash https://raw.github.com/git/git/master/contrib/completion/git-completion.bash
+curl -Sso ~/dotfiles/vim/colors/solarized.vim https://raw.github.com/altercation/vim-colors-solarized/master/colors/solarized.vim
