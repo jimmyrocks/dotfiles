c120c7e782e1b5997eb632cade3e7003721b6db3
Jim McAndrew <jim@loc8.us>
2012-12-04 17:15:10 -0700

    Added pyflakes-vim and some command line corrections

---
+++ bash/scripts/customfunctions.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# These are helpers I've added to remind me that I'm doing something wrong
+
+function ssh {
+    if `echo $1 | grep @ 1>/dev/null 2>1`; then
+       `which ssh` $1
+    else
+        echo "You did not include a username"
+        echo -n "Did you mean: ssh $USER@$1 (y/n): "
+        read yn
+        if [[ $yn == "Y" || $yn == "y" ]]; then
+            `which ssh` $USER@$1
+        elif [[ $yn == "N" || $yn == "n" ]]; then
+            echo -n "Which username did you intend to use?: "
+            read new_un
+            echo "ssh" $new_un@$1
+            `which ssh` $new_un@$1
+        fi
+    fi
+}
+
+function sftp {
+    if `echo $1 | grep @ 1>/dev/null 2>1`; then
+       `which sftp` $1
+    else
+        echo "You did not include a username"
+        echo -n "Did you mean: sftp $USER@$1 (y/n): "
+        read yn
+        if [[ $yn == "Y" || $yn == "y" ]]; then
+            `which sftp` $USER@$1
+        elif [[ $yn == "N" || $yn == "n" ]]; then
+            echo -n "Which username did you intend to use?: "
+            read new_un
+            echo "sftp" $new_un@$1
+            `which sftp` $new_un@$1
+        fi
+    fi
+}
+
+function vf {
+    params=$1
+    echo -n "Did you mean: 'cd $params'? ((y)/n): "
+    read yn
+    if [[ $yn == "N" || $yn == "n" ]]; then
+        echo "ok"
+    else
+        cd $params
+    fi
+}
+
