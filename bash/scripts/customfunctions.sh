3843518153ba1d00613b11b5717445e058e27850
Jim McAndrew <jim@loc8.us>
2013-11-15 14:02:11 -0700

    Update IP in serveHttp script

--- bash/scripts/customfunctions.sh
+++ bash/scripts/customfunctions.sh
@@ -50,6 +50,6 @@ function vf {
 }
 
 function serveHttp {
-  /sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'
+  ifconfig | awk -v RS="\n\n" '{ for (i=1; i<=NF; i++) if ($i == "inet" && $(i+1) ~ /^addr:/) address = substr($(i+1), 6); if (address != "127.0.0.1") printf "\033[34m%s\033[0m\t\033[33m%s\033[0m\n", $1, address }'
   python -m SimpleHTTPServer $1
 }
