af73759e0a56177d626e9f24483378240483816f
Jim McAndrew <jim@loc8.us>
2013-12-23 13:25:09 -0700

    Add thisip command

--- bash/scripts/customfunctions.sh
+++ bash/scripts/customfunctions.sh
@@ -50,6 +50,10 @@ function vf {
 }
 
 function serveHttp {
-  ifconfig | awk -v RS="\n\n" '{ for (i=1; i<=NF; i++) if ($i == "inet" && $(i+1) ~ /^addr:/) address = substr($(i+1), 6); if (address != "127.0.0.1") printf "\033[34m%s\033[0m\t\033[33m%s\033[0m\n", $1, address }'
+  thisip
   python -m SimpleHTTPServer $1
+}
+
+function thisip {
+  ifconfig | awk -v RS="\n\n" '{ for (i=1; i<=NF; i++) if ($i == "inet" && $(i+1) ~ /^addr:/) address = substr($(i+1), 6); if (address != "127.0.0.1") printf "\033[34m%s\033[0m\t\033[33m%s\033[0m\n", $1, address }'
 }
