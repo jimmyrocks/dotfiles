1c597c5a48b2bbc654e541dbf11897ce460f4c99
Jim McAndrew <jim@loc8.us>
2014-03-11 09:29:44 -0600

    Add update geo script to get the latest version of gdal

---
+++ update_geo.sh
@@ -0,0 +1,47 @@
+script_pwd=`pwd`
+install_esri='false'
+echo '---------------------------'
+echo 'Installing gdal with FileGDB Support'
+echo '---------------------------'
+if [install_esri == 'true']; then
+  if [ `uname -m` = "x86_64" ]; then
+    echo "Downloading FileGDB_API_1_3-64.tar.gz from ESRI"
+    curl -o ~/tmp/FileGDB_API_1_3.tar.gz http://downloads2.esri.com/Software/FileGDB_API_1_3-64.tar.gz
+  else
+    echo "Downloading FileGDB_API_1_3-32.tar.gz from ESRI"
+    curl -o ~/tmp/FileGDB_API_1_3.tar.gz http://downloads2.esri.com/Software/FileGDB_API_1_3-32.tar.gz
+  fi
+
+  echo '---------------------------'
+  echo 'Extracting and setting params'
+  echo '---------------------------'
+  # http://hydrogeotools.blogspot.com/2013/07/install-gdal-with-file-gdb-and.html
+  mkdir -p /usr/local/ # not sure why you wouldn't have this?
+  tar -zxvf ~/tmp/FileGDB_API_1_3.tar.gz -C /usr/local/
+  rm ~/tmp/FileGDB_API_1_3.tar.gz
+
+  # Add the line to ldconfig
+  if grep -Fxq "/usr/local/FileGDB_API/lib" /etc/ld.so.conf; then
+    echo "/usr/local/FileGDB_API/lib" >> /etc/ld.so.conf
+    ldconfig
+  fi
+fi
+echo '---------------------------'
+echo 'Installing gdal'
+echo '---------------------------'
+# Get gdal!
+apt-get remove gdal-bin #In case you have an older verion
+svn checkout https://svn.osgeo.org/gdal/trunk/gdal ~/tmp/gdal
+cd ~/tmp/gdal
+./configure --with-python --with-openjpeg --with-geotiff --with-jpeg --with-png --with-expat --with-libkml --with-fgdb=/usr/local/FileGDB_API --with-ogdi=no --with-openjpeg --with-geos=yes
+make
+make install
+ldconfig
+
+# All done, run tests
+echo '---------------------------'
+echo 'Running Tests'
+echo '---------------------------'
+gdal-config --version
+gdal-config --formats
+ogrinfo --formats | grep 'FileGDB'
