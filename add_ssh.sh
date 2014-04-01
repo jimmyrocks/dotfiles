c0c071cc6973994dec94c6ccf6e9ddbb789be1dc
Jim McAndrew <jim@loc8.us>
2014-04-01 14:48:55 -0600

    added symlinks

--- add_ssh.sh
+++ add_ssh.sh
@@ -4,4 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 eval `ssh-agent -s`
 sudo chmod 600 $DIR/id_rsa
 sudo chmod 600 $DIR/id_rsa.pub
-ssh-add $DIR/id_rsa
+ln -s $DIR/id_rsa ~/.ssh/github_id_rsa
+ln -s $DIR/id_rsa.pub ~/.ssh/github_id_rsa.pub
+ssh-add ~/.ssh/github_id_rsa`
