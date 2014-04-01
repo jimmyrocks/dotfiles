379c504fb1b77b65ec71be6c6a6f600cd53b6d33
Jim McAndrew <jim@loc8.us>
2014-04-01 15:29:06 -0600

    fix up file

--- add_ssh.sh
+++ add_ssh.sh
@@ -4,6 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 eval `ssh-agent -s`
 sudo chmod 600 $DIR/id_rsa
 sudo chmod 600 $DIR/id_rsa.pub
-ln -s $DIR/id_rsa ~/.ssh/github_id_rsa
-ln -s $DIR/id_rsa.pub ~/.ssh/github_id_rsa.pub
-ssh-add ~/.ssh/github_id_rsa`
+ssh-add $DIR/id_rsa.pub
