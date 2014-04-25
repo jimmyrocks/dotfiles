76a47ead313a25851220e69f73bde154c8e1d58f
Jim McAndrew <jim@loc8.us>
2014-04-25 10:18:20 -0600

    Do a pull or a clone depending if we need to or not

--- git_install.sh
+++ git_install.sh
@@ -1,2 +1,5 @@
-git clone ssh://jimmyrocks@loc8.us/home/jimmyrocks/private/github_ssh.git ~/.ssh/github_ssh/
-bash ~/.ssh/github_ssh/add_ssh.sh
+if [ ! -d "$HOME/.ssh/github_ssh/" ]; then
+  git clone ssh://jimmyrocks@loc8.us/home/jimmyrocks/private/github_ssh.git $HOME/.ssh/github_ssh/
+fi
+git --git-dir=$HOME/.ssh/github_ssh/.git pull
+bash $HOME/.ssh/github_ssh/add_ssh.sh
