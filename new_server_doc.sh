fe231103397c43d3c39497830ce9d351a368549b
Jim McAndrew <jim@loc8.us>
2019-01-04 20:50:40 +0000

    use relative link for node install

--- new_server_doc.sh
+++ new_server_doc.sh
@@ -70,5 +70,4 @@ sudo npm cache clean -f
 sudo npm install -g n
 sudo n stable
 
-sudo ln -sf /usr/local/n/versions/node/<VERSION>/bin/node /usr/bin/node
-
+sudo ln -sf $(n bin stable) /usr/bin/node
