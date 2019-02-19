23c58a104ffd3cd1998ff94be828e4144a680668
Jim McAndrew <jim@loc8.us>
2019-02-18 21:33:12 -0500

    Update the custom scripts

--- bash/scripts/custom-docker-functions.sh
+++ bash/scripts/custom-docker-functions.sh
@@ -1,40 +1,28 @@
 # Docker functions
-function dc.bash() {
-  docker-compose exec $1 bash
-}
-
-function dc.bash() {
-  sudo docker-compose exec $1 bash
-}
-
-function sdc.load() {
-  filename=${$1:='docker-compose.yml'}
-  sudo docker-compose build -f $filename
-  sudo docker-compose up -f $filename
-}
-
-function sdc.reload() {
-  filename=${$1:='docker-compose.yml'}
-  sudo docker-compose down -f $filename
-  sudo dc.load $filename
-}
 
 function dc.load() {
-  filename=${$1:='docker-compose.yml'}
-  docker-compose build -f $filename
-  docker-compose up -f $filename
+  filename=$1
+  filename=${filename:='docker-compose.yml'}
+  sudo docker-compose -f $filename build
+  sudo docker-compose -f $filename up
 }
 
 function dc.reload() {
-  filename=${$1:='docker-compose.yml'}
-  docker-compose down -f $filename
+  filename=$1
+  filename=${filename:='docker-compose.yml'}
+  dc.down $filename
   dc.load $filename
 }
 
-function sdc.ps() {
+function dc.down() {
+  filename=$1
+  filename=${filename:='docker-compose.yml'}
+  sudo docker-compose -f $filename down
+}
+
+function dc.ps() {
   sudo docker-compose ps
 }
 
 # Docker aliases
-dc='docker-compose'
-sdc='sudo docker-compose'
+dc='sudo docker-compose'
