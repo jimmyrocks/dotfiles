ea7a3aeb34ef6b7724c685a1094c41a379bba712
Jim McAndrew <jim@loc8.us>
2019-10-08 09:31:00 -0600

    Update ip function

--- bash/scripts/customfunctions.sh
+++ bash/scripts/customfunctions.sh
@@ -76,7 +76,8 @@ function serveHttp {
 }
 
 function thisip {
-  ifconfig | awk -v RS="\n\n" '{ for (i=1; i<=NF; i++) if ($i == "inet" && $(i+1) ~ /^addr:/) address = substr($(i+1), 6); if (address != "127.0.0.1") printf "\033[34m%s\033[0m\t\033[33m%s\033[0m\n", $1, address }'
+  # ifconfig | awk -v RS="\n\n" '{ for (i=1; i<=NF; i++) if ($i == "inet" && $(i+1) ~ /^addr:/) address = substr($(i+1), 6); if (address != "127.0.0.1") printf "\033[34m%s\033[0m\t\033[33m%s\033[0m\n", $1, address }'
+  ip address | grep eth0 | grep inet | perl -pe 's/.+?inet\s(.+?)\/.+/\1/g'
 }
 
 function resetip {
